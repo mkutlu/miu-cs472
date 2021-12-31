d700262a645b5ba79bde4ad6738ce7f05d7eb16d
Mesut Kutlu <mkutlu13@gmail.com>
2021-12-31 05:51:55 -0600

    Lab2-forms corrections for php and css styling.

--- lab2-forms/signup.php
+++ lab2-forms/signup.php
@@ -1,13 +1,9 @@
 <!DOCTYPE html>
-<!--[if lt IE 7]>
-<html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>
-<html class="no-js lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>
-<html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]>
-<html class="no-js"> <![endif]-->
-<html>
+<!--[if lt IE 7]><html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]><html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]><html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><html class="no-js"> <![endif]-->
+<html lang="en">
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -17,54 +13,54 @@
     <link rel="stylesheet" href="styles/style.css">
 </head>
 <body>
-<!--[if lt IE 7]>
-<p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a>
-    to improve your experience.</p>
-<![endif]-->
+<!--[if lt IE 7]><p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a>to improve your experience.</p><![endif]-->
 <img src="images/nerd.jpg" alt="logo" width="250">
 <div class="signup-container">
     <div class="signup-header">New User Signup</div>
-    <form id="student-form" action="params.php" method="post" target="formSending">
-        <label for="name">Name:</label>
-        <input type="text" id="name" name="name" value=""> <br>
-        <label for="id">Gender:</label>
-        <label>
-            <input type="radio" value="1" name="entry">
-            Male
-        </label>
-        <label>
-            <input type="radio" value="2" name="entry">
-            Female
-        </label>
-        <label>
-            <input type="radio" value="3" name="entry">
-            Other
-        </label>
-        <label>
-            <input type="radio" value="4" name="entry">
-            Prefer not to say
-        </label>
-        <br>
-        <label for="age">Age:</label>
-        <input type="number" id="age" name="age" value=""><br>
-        <label for="personality">Personality Type:</label>
-        <input type="text" id="personality" name="personality" value=""><br>
-        <label for="os">Favourite OS:</label>
-        <select name="os" id="os">
-            <option value=""></option>
-            <option value="Windows">Windows</option>
-            <option value="Macos">Mac OS</option>
-            <option value="Linux">Linux</option>
-            <option value="Centos">Centos</option>
-        </select><br>
+    <div class="signup-labels">
+        <label for="name">Name:</label><br>
+        <label for="gender">Gender:</label><br>
+        <label for="age">Age:</label><br>
+        <label for="personality">Personality Type:</label><br>
+        <label for="os">Favourite OS:</label><br>
         <label for="min">Seeking Age:</label>
-        <input type="number" id="min" name="min" value="">
-        <input type="number" id="max" name="max" value="">
-        <br>
-        <input type="submit" value="Sign Up">
-    </form>
+    </div>
+    <div class="signup-inputs">
+        <form id="student-form" action="params.php" method="post">
+            <input type="text" id="name" name="name" value="" maxlength="16"> <br>
+            <label>
+                <input type="radio" value="1" id="gender" name="gender">
+                Male
+            </label>
+            <label>
+                <input type="radio" value="2" id="gender"  name="gender" checked>
+                Female
+            </label>
+            <label>
+                <input type="radio" value="3" id="gender"  name="gender">
+                Other
+            </label>
+            <label>
+                <input type="radio" value="4" id="gender" name="gender">
+                Prefer not to say
+            </label>
+            <br>
+            <input type="text" pattern="\d*" maxlength="2" size="6" id="age" name="age" value=""><br>
+            <input type="text" id="personality" name="personality" value="" size="6"> <a href="http://www.humanmetrics.com/cgi-win/JTypes2.asp">(Don't know your type?)</a><br>
+            <select name="os" id="os">
+                <option value="Windows" selected>Windows</option>
+                <option value="Macos">Mac OS</option>
+                <option value="Linux">Linux</option>
+                <option value="Centos">Centos</option>
+            </select>
+            <br>
+            <input type="text" id="min" name="min" value="" placeholder="min" pattern="\d*" maxlength="2" size="6" > to
+            <input type="text" id="max" name="max" value="" placeholder="max" pattern="\d*" maxlength="2" size="6" >
+            <br>
+            <input type="submit" value="Sign Up">
+        </form>
+    </div>
 </div>
-
 <p>
     This page for dingle nerds to meet and date each other! Type in <br>
     your personal information and wait for the nerdly luv to begin! <br>
