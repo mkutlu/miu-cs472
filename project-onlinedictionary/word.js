787166988d20a13be53ebfd9943d670f2746b71c
Mesut Kutlu <mkutlu13@gmail.com>
2022-01-29 20:49:16 -0600

    project-onlinedictionary updated.

---
+++ project-onlinedictionary/word.js
@@ -0,0 +1,16 @@
+var mysql = require('mysql')
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'mkutlu',
+    password: 'Premium2019!',
+    database: 'entries'
+})
+connection.connect()
+exports.searchTerm = function(term, res) {
+    connection.query("SELECT * FROM entries WHERE word = " + connection.escape(term), function(err, rows, fields) {
+        if (err) throw err
+
+        console.log('The solution is: ', rows);
+        res.json(rows);
+    })
+}
