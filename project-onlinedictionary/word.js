6dce42f152d5a5e269a9a209193113dc1de48e33
Mesut Kutlu <mkutlu13@gmail.com>
2022-01-29 21:08:52 -0600

    project-onlinedictionary updated.

--- project-onlinedictionary/word.js
+++ project-onlinedictionary/word.js
@@ -9,8 +9,6 @@ connection.connect()
 exports.searchTerm = function(term, res) {
     connection.query("SELECT * FROM entries WHERE word = " + connection.escape(term), function(err, rows, fields) {
         if (err) throw err
-
-        console.log('The solution is: ', rows);
         res.json(rows);
     })
 }
